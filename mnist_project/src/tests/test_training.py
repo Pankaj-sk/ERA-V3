ba7a3d44d5f3f0f29dcf0864c4a56bf710794b5e
Pankaj-sk <pankaj.suresh_kumar@okstate.edu>
2024-12-05 16:32:20 -0700

    Second commit after file structure fix

--- mnist_project/src/tests/test_training.py
+++ mnist_project/src/tests/test_training.py
@@ -1,9 +1,16 @@
 import torch
+import glob
 from src.model import MNISTModel
 from src.utils import evaluate_model
 
 def test_model_accuracy():
     model = MNISTModel()
-    model.load_state_dict(torch.load('model_mnist_latest.pth'))
+    # Find the most recent model file
+    model_files = glob.glob('model_mnist_*.pth')
+    if not model_files:
+        raise FileNotFoundError("No model file found")
+    latest_model = max(model_files)  # Gets the most recent file
+    
+    model.load_state_dict(torch.load(latest_model))
     accuracy = evaluate_model(model)
     assert accuracy >= 0.8, f"Model accuracy {accuracy} is below 0.8" 
